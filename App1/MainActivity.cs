a63d4b2f2dc0eb5d4b21dfb382acc5768cad32cf
steven <steven0529@gmail.com>
2015-03-25 17:44:30 +0800

    關於Linker設定

---
+++ App1/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Reflection;
+using System.Linq;
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            typeof(Button).GetProperty("Text").SetValue(button, "test Linker");
+        }
+    }
+
+
+
+}
+
